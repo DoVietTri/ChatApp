ca4f9d0c2ac5edde365b48f1d1f0ed81a4805900
Do Viet Tri <doviettri27091999@gmail.com>
2020-09-12 15:24:14 +0700

    #16_TriDV_code: add new contact handle real time

--- src/public/js/removeRequestContact.js
+++ src/public/js/removeRequestContact.js
@@ -11,8 +11,24 @@ function removeRequestContact() {
                     $('#find-user').find(`div.user-remove-request-contact[data-uid = ${targetId}]`).hide();
                     $('#find-user').find(`div.user-add-new-contact[data-uid = ${targetId}]`).css('display', 'inline-block');          
                     decreaseNumberNotifyContact('count-request-contact-sent');
+
+                     //real time
+                    socket.emit('remove-request-contact', {contactId: targetId});
                 }
             }
         });
     });
 }
+
+socket.on('response-remove-request-contact', function(user) {
+    
+    $('.noti_content').find(`span[data-uid=${user.id}]`).remove();
+
+    //remove 
+    
+
+    decreaseNumberNotifyContact('count-request-contact-received');
+
+    decreaseNumberNotification('noti_contact_counter');
+    decreaseNumberNotification('noti_counter');
+});
