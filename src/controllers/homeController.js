877ac7b1d00cdc12d11dc93d0e8cc337950035b0
Do Viet Tri <doviettri27091999@gmail.com>
2020-09-19 10:40:42 +0700

    #21_TriDV_code: render contact view

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,18 +1,42 @@
-import {notification} from './../services/index';
+import {notification, contact} from './../services/index';
 
-let getHome =async (req, res) => {
-    //only 10 items one time
+let getHome = async (req, res) => {
+    //get notifications (5 item on time)
     let notifications  = await notification.getNotifications(req.user._id);
 
     //get amount notification unread
     let countNotifUnread = await notification.countNotifUnread(req.user._id);
+    
+    //get contacts (5 item on time)
+    let contacts = await contact.getContacts(req.user._id);
+
+    //count all contacts
+    let countAllContacts = await contact.countAllContacts(req.user._id);
+
+    //get contacts sent (5 item on time)
+    let contactsSent = await contact.getContactsSent(req.user._id);
+
+    //count all contacts sent
+    let countAllContactsSent = await contact.countAllContactsSent(req.user._id);
+
+    //get contacts received (5 item on time)
+    let contactsReceived = await contact.getContactsReceived(req.user._id);
+
+    //count all contacts received
+    let countAllContactsReceived = await contact.countAllContactsReceived(req.user._id);
     
     res.render('main/home/home', {
         errors: req.flash('errors'),
         success: req.flash('success'),
         user  : req.user,
         notifications: notifications,
-        countNotifUnread: countNotifUnread
+        countNotifUnread: countNotifUnread,
+        contacts: contacts,
+        contactsSent: contactsSent,
+        contactsReceived: contactsReceived,
+        countAllContacts: countAllContacts,
+        countAllContactsSent: countAllContactsSent,
+        countAllContactsReceived: countAllContactsReceived
     });
 };
 
